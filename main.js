e894f6461b1cdc1927d9e53e430ab7816f1c9665
Thomas <86979686+Jomm95@users.noreply.github.com>
2022-02-21 17:06:25 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+const vue = new Vue(
+    {
+    el: '#app',
+    data: {
+        newtodo :"",
+        todos : ["magnà", "bere", "dormire"],
+      
+    },
+    method: {
+        addToDo () {
+            this.todos.push(this.newtodo);
+            this.newtodo="";
+        },
+    }
+  }
+);
