60f463be9ab9b330f6dc25880e842a28c7e0d0cb
Thomas <venturini.thomas95@gmail.com>
2022-02-22 23:36:29 +0100

    concluso no bonus

--- main.js
+++ main.js
@@ -3,13 +3,48 @@ const vue = new Vue(
     el: '#app',
     data: {
         newtodo :"",
-        todos : ["magnà", "bere", "dormire"],
+        todos : [
+            {
+                text: "cucinare",
+                done:false
+            },
+            {
+                text: "fare la spesa",
+                done:false
+            },
+            {
+                text: "studiare",
+                done:false
+            },
+            {
+                text: "palestra",
+                done:false
+            }
+        ]
       
     },
-    method: {
-        addToDo() {
-            this.todos.push(this.newtodo);
-            this.newtodo="";
+    methods: {
+        addToDo(){
+            if(this.newtodo.trim().length > 0){
+                this.todos.push(
+                    {
+                        text: this.newtodo.trim(),
+                        done: false,
+                    }
+                    )
+                }
+                this.newtodo="";
+            
+    },
+
+        removeToDo(index) {
+            this.todos.splice(index, 1);
+
+
+        },
+
+        throughLine (index){
+            this.todos[index].done = !this.todos[index].done;
         },
     }
   }
